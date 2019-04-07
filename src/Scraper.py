fcccd09618f010fa3bf855e0258c349f88796255
cesariva <48776506+cesariva@users.noreply.github.com>
2019-04-06 21:37:33 -0500

    Update Scraper.py
    Scraper flujo base desde un plugin inicial a un plugin final

--- src/Scraper.py
+++ src/Scraper.py
@@ -6,11 +6,12 @@ import io
 import csv
 import os
 #Para realizar las consultas HTTP
-import requests
+#import requests
 #Para realizar el parsing del HTML
-from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup
 #Para interpretar el json
 import json
+import time
 
 class Scrapper:
     def __init__(self):	
@@ -21,58 +22,49 @@ class Scrapper:
       config = ConfigParser.RawConfigParser(allow_no_value=True)
       config.readfp(io.BytesIO(configf))
       # Print config contents
-      print("Base URL to scraping:")
-      print(config.get('target', 'base_url'))
-      print("Time to wait between queries")
-      print(config.get('general', 'query_delay')+" seconds")
-      print("Storage file")
-      print(config.get('target', 'scrap_file'))
       self.filename=config.get('target', 'scrap_file')
-      self.delay=config.get('general', 'query_delay')
+      self.tdelay=config.get('general', 'query_delay')
       self.base_url=config.get('target', 'base_url')
+      self.initial=int(config.get('target', 'initial_plugin'))
+      self.final=int(config.get('target', 'final_plugin'))
+      print("Base URL to scraping:"+self.base_url)
+      print("Time to wait between queries:"+self.tdelay+" seconds")
+      print("Storage file"+self.filename)
+      print("Plugins del "+str(self.initial)+" hasta el "+str(self.final))
+      self.counter=self.initial
 
     def load_file(self):
       exists=os.path.isfile(self.filename)
       if exists:
       	with open(self.filename, mode='r') as csv_file:
           csv_reader = csv.DictReader(csv_file)
+        csv_file.close()
           
     def scrap_obs(self):
-      print("Scraping obs")
-      #Pagina a consultar
-      page=requests.get("https://www.tenable.com/plugins/nessus/421")
-      #verifica el codigo de respuesta.
-      page.status_code	
-      #Almacena el contenido de la pagina
-      soup=BeautifulSoup(page.content)
-      print(soup.h1)
-      #Analizando el contendio de la pagina se identifico una estructura en json 
-      #que se incluye al fina de la pagina bajo el id "NEXT_DATA"
-      tenab_json=soup.find(id="__NEXT_DATA__").text
-      print(tenab_json)
-      #Convierte el contenido json en un diccionario se que pueda 
-      newDictionary=json.loads(tenab_json)
-      newDictionary
+      print("Scraping obs"+str(self.counter))
+      
     	
     def store_obs(self):
-      print("scraping Obs")
+      print("storing Obs")
     
-    
-    	
+     	
     def check_end(self):
       print("Cheking end")
-    	
-      
+      if self.final>self.counter:
+        self.counter+=1
+        return True
+      return False
+
+    def ex_delay(self):
+      print("Delay") 	
+      time.sleep(float(self.tdelay))
 
 
 s=Scrapper()
 s.load_file()
+while s.check_end():
+   s.scrap_obs()
+   s.store_obs()
+   s.ex_delay()
+   
 
-    def check_end(self):
-      print("Cheking end")
-    	
-      
-
-
-s=Scrapper()
-s.load_file()
