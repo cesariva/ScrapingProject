19d5f49db1d8687d3fa9c9fa3d6b3194ab0ead08
cesariva <48776506+cesariva@users.noreply.github.com>
2019-04-15 14:02:50 -0500

    Update Scraper.py
    Mejorar los comentarios del script.

--- src/Scraper.py
+++ src/Scraper.py
@@ -11,17 +11,27 @@ import requests
 from bs4 import BeautifulSoup
 #Para interpretar el json
 import json
+#Para configurar retardos de tiempo
 import time
 
 class Scrapper:
-    def __init__(self):	
+"""Clase base de Scraping que tiene la informacion básica para configurar, 
+cargar el archivo, realizar el scraping, guardar la observacion, y esperar
+un tiempo. 
+Este script se debe invocar como Scraper.py AchivoConfiguracion
+Ejemplo: Scraper.py TenableConfig.txt
+"""
+
+    def __init__(self):
+        """ Incicializacion del scraper con el archivo de configuracion dado en el primer argumento"""
       print("Inicio Scraper")
-      # Load the configuration file in the first argument
+      # Load the configuration filename in the first argument
       with open(sys.argv[1]) as f:
           configf = f.read()
+      #Utiliza la clase configparser para interperetar el archivo de configuracion
       config = ConfigParser.RawConfigParser(allow_no_value=True)
       config.readfp(io.BytesIO(configf))
-      # Print config contents
+      # Inicializa el contenido de las variables de la clase con el archivo de configuracion
       self.filename=config.get('target', 'scrap_file')
       self.tdelay=config.get('general', 'query_delay')
       self.base_url=config.get('target', 'base_url')
@@ -35,6 +45,7 @@ class Scrapper:
       self.listObs=[]
 
     def load_file(self):
+        """ Funcion que carga el archivo donde se almacena el dataset"""
       exists=os.path.isfile(self.filename)
       if exists:
         with open(self.filename, mode='r') as csv_file:
@@ -42,6 +53,7 @@ class Scrapper:
         csv_file.close()
           
     def scrap_obs(self):
+        """ Funcion que descarga la informacion especifica de la pagina WEB y la almacena temporalmente en memoria"""
       print("Scraping obs: "+str(self.counter))
       #Pagina a consultar
       page=requests.get(self.base_url+str(self.counter))
@@ -57,6 +69,7 @@ class Scrapper:
       self.listObs.append(newDictionary['props']['pageProps']['plugin'])
     	
     def store_obs(self):
+        """ Funcion de almacena en disco en formato CVS los datos que se tienen en memoria"""
       print("storing Obs")
       fields=set()
       for i in self.listObs:
@@ -66,9 +79,8 @@ class Scrapper:
           w.writeheader()
           w.writerows(self.listObs)
       
-    
-     	
     def check_end(self):
+        """ Funcion que verifica si ya se llego al final del scraping"""
       print("Cheking end")
       if self.final>self.counter:
         self.counter+=1
@@ -76,15 +88,23 @@ class Scrapper:
       return False
 
     def ex_delay(self):
+        """ Función que ejecuta un retardo """
       print("Delay") 	
       time.sleep(float(self.tdelay))
 
-
+""" Codigo principal del scraper """
+#Inicializa la clase
 s=Scrapper()
+#Carga el archivo
 s.load_file()
+# Hace un bucle hasta que se cumpla la condición de fin de scraping
 while s.check_end():
+   # Hace la consutla especifica en la página WEB y guarda el resutlado en memoria 
    s.scrap_obs()
+   # Graba el valor consultado en archivo CSV, en el disco. 
    s.store_obs()
+   # Ejecuta un retardo antes de realizar una nueva consulta a la página WEB. 
    s.ex_delay()
    
+
 
